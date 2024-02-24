f1f29580e2f7f3dc4212cb4135ffaa31fbe641db
Tiago Lopes <tiago.lopes2905@gmail.com>
2024-02-24 15:28:58 -0300

    add project

---
+++ src/test/java/com/project/professor/allocation/btf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.btf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
