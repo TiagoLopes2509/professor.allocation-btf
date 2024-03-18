67272e0f8d03bfe5e8da909d09600f21db936403
Tiago Lopes <tiago.lopes2905@gmail.com>
2024-03-17 22:08:38 -0300

    criação da entidade department

---
+++ src/main/java/com/project/professor/allocation/btf/entity/Department.java
@@ -0,0 +1,44 @@
+package com.project.professor.allocation.btf.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "department")
+public class Department {
+		
+	public Department() 
+	{
+
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name", unique = true, nullable = false)
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
+	
+}
