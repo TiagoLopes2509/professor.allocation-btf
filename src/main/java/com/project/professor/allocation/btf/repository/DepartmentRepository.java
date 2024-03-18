1555377c9b74dc9f2f968b008290505e4391ea92
Felipe Cavallini Sakay <felipe.sakay@partners.mv.com.br>
2024-03-18 15:51:51 -0300

    Criação dos repositorios

---
+++ src/main/java/com/project/professor/allocation/btf/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.project.professor.allocation.btf.repository;
+
+import com.project.professor.allocation.btf.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
+
