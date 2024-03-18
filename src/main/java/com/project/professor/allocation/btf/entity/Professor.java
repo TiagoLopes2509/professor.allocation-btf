72aaa255f56b568c4c873276032337e2172e3cd7
Tiago Lopes <tiago.lopes2905@gmail.com>
2024-03-17 22:10:12 -0300

    Criação da entidade Professor

---
+++ src/main/java/com/project/professor/allocation/btf/entity/Professor.java
@@ -0,0 +1,53 @@
+package com.project.professor.allocation.btf.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "professor")
+public class Professor {
+	
+	public Professor() 
+	{
+
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "cpf", unique = true, nullable = false, length = 11)
+	private String cpf;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	@Override
+	public String toString() {
+		return "Professor [id=" + id + ", name=" + name + ", cpf=" + cpf + "]";
+	}
+
+}
