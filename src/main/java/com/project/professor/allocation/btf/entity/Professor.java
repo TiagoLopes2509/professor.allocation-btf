4b983dbe4e6368b75eccca651f07ff6ef8df424d
Tiago Lopes <tiago.lopes2905@gmail.com>
2024-03-17 22:13:59 -0300

    adição do relacionamento das tabelas

--- src/main/java/com/project/professor/allocation/btf/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/btf/entity/Professor.java
@@ -1,10 +1,14 @@
 package com.project.professor.allocation.btf.entity;
 
+import com.project.professor.alocation.entity.Department;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +29,10 @@ public class Professor {
 	
 	@Column(name = "cpf", unique = true, nullable = false, length = 11)
 	private String cpf;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "department_id", nullable = false)
+	private Department department;
 	
 	public Long getId() {
 		return id;
