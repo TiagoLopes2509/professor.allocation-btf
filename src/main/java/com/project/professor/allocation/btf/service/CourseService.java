85c9e01e25d15f6b0d1a18aaa5d3b887cc9ab871
Bruno Léo Silva <brunoleosilva@hotmail.com>
2024-04-02 21:32:51 -0300

    service class

---
+++ src/main/java/com/project/professor/allocation/btf/service/CourseService.java
@@ -0,0 +1,55 @@
+package com.project.professor.allocation.btf.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.project.professor.allocation.btf.repository.CourseRepository;
+import com.project.professor.allocation.btf.entity.Course;
+
+@Service
+public class CourseService {
+
+	private final CourseRepository courseRepository;
+	
+	public CourseService(CourseRepository courseRepository) {
+		super();
+		this.courseRepository = courseRepository;
+	}
+	
+	public List<Course> findAll(String name) {
+		return name == null ? courseRepository.findAll() : courseRepository.findByNameIgnoreCaseContaining(name);
+	}
+	
+	public Course findById(Long id) {
+		return courseRepository.findById(id).orElse(null);
+	}
+	
+	public Course create(Course course) {
+		return saveInternal(course);
+	}
+	
+	public Course update(Course course) {
+		boolean exist = courseRepository.existsById(course.getId());
+		if (exist) {
+			return saveInternal(course);
+		}
+		return null;
+	}
+	
+	private Course saveInternal(Course course) {
+		return courseRepository.save(course);
+	}
+	
+	public boolean delete(Course course) {
+		boolean exist = courseRepository.existsById(course.getId());
+		if (exist) {
+			courseRepository.delete(course);
+		}
+		return exist;
+	}
+	
+	public void deleteAll() {
+		courseRepository.deleteAllInBatch();
+	}
+}
