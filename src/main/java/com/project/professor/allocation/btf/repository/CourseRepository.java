88a777e1690b9a3300efdfcd4831eedcc4a336f7
Bruno Léo Silva <brunoleosilva@hotmail.com>
2024-04-02 21:32:05 -0300

    update repository

--- src/main/java/com/project/professor/allocation/btf/repository/CourseRepository.java
+++ src/main/java/com/project/professor/allocation/btf/repository/CourseRepository.java
@@ -1,9 +1,19 @@
 package com.project.professor.allocation.btf.repository;
 
-import com.project.professor.allocation.btf.entity.Course;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.professor.allocation.btf.entity.Course;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+	Course findByName(String name);
+	
+	List<Course> findByNameLike(String name);
+	
+	List<Course> findByNameContaining(String name);
+	
+	List<Course> findByNameIgnoreCaseContaining(String name);
 }
 
--- src/main/java/com/project/professor/allocation/btf/repository/DepartmentRepository.java
+++ src/main/java/com/project/professor/allocation/btf/repository/DepartmentRepository.java
@@ -1,9 +1,19 @@
 package com.project.professor.allocation.btf.repository;
 
 import com.project.professor.allocation.btf.entity.Department;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+	Department findByName(String name);
+	
+	List<Department> findByNameLike(String name);
+	
+	List<Department> findByNameContaining(String name);
+	
+	List<Department> findByNameIgnoreCaseContaining(String name);
 }
 
